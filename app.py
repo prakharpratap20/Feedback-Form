f5a0e49313904cc8ea31652bf691c9ccdd1f2acb
Prakhar Pratap <prakharpratap2001@gmail.com>
2024-02-25 00:08:10 +0530

    adding database

--- app.py
+++ app.py
@@ -3,6 +3,35 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+ENV = "dev"
+
+if ENV == "dev":
+    app.debug = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Hola@migo@localhost/feedback" 
+else:
+    app.debug = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = "" 
+    
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Feedback(db.Model):
+    __tablename__ = "feedback"
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(200), unique=True)
+    project = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    feedback = db.Column(db.Text())
+    
+    def __init__(self, user, project, rating, feedback):
+        self.user = user
+        self.project = project
+        self.rating = rating
+        self.feedback = feedback
+        
+    
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -21,5 +50,4 @@ def submit():
         return render_template("success.html")
 
 if __name__ == "__main__":
-    app.debug = True
     app.run()
